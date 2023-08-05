49e7ec17112c70413d9427c8c5d654fc8860ba2a
You <you@gmail.com>
2023-08-05 12:59:11 +0200

    ajout html, js

---
+++ navbar.js
@@ -0,0 +1,31 @@
+
+function navbar(){
+html ='';
+html += ' <nav class="navbar navbar-expand-sm bg-light justify-content-center">';
+html += '  <ul class="navbar-nav">';
+html += '    <li class="nav-item"><a class="nav-link" href="page1.html">page1</a></li>';
+html += '    <li class="nav-item"><a class="nav-link" href="page2.html">page2</a></li>';
+html += '    <li class="nav-item"><a class="nav-link" href="page3.html">page3</a></li>';
+html += '    <li class="nav-item"><a class="nav-link" href="page4.html">page4</a></li>';
+html += '    <li class="nav-item"><a class="nav-link" href="page6.html">page6</a></li>';
+html += '    <li class="nav-item"><a class="nav-link" href="page7.html">page7</a></li>';
+html += '    <li class="nav-item"><a class="nav-link" href="page11.html">page11</a></li>';
+html += '    <li class="nav-item"><a class="nav-link" href="page12.html">page12</a></li>';
+html += '    <li class="nav-item"><a class="nav-link" href="page14.html">page14</a></li>';
+html += '    <li class="nav-item"><a class="nav-link" href="page16.html">page16</a></li>';
+html += '    <li class="nav-item"><a class="nav-link" href="page17.html">page17</a></li>';
+html += '    <li class="nav-item"><a class="nav-link" href="page20.html">page20</a></li>';
+html += '    <li class="nav-item"><a class="nav-link" href="page21.html">page21</a></li>';
+html += '    <li class="nav-item"><a class="nav-link" href="page22.html">page22</a></li>';
+html += '    <li class="nav-item"><a class="nav-link" href="page23.html">page23</a></li>';
+html += '    <li class="nav-item"><a class="nav-link" href="page24.html">page24</a></li>';
+html += '    <li class="nav-item"><a class="nav-link" href="page26.html">page26</a></li>';
+html += '    <li class="nav-item"><a class="nav-link" href="page27.html">page27</a></li>';
+html += '    <li class="nav-item"><a class="nav-link" href="page28.html">page28</a></li>';   
+html += '  </ul>';
+html += '</nav>';
+document.write(html);
+}
+
+
+ 
